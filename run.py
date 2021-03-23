9e2a51edcbd872216b806741ceaa79102802d0c6
Wilson Sanmiguel Guerrero <wsg20043@hotmail.com>
2021-03-23 09:06:21 -0500

    first commit

---
+++ run.py
@@ -0,0 +1,20 @@
+print(2+2)
+print(2*3)
+print(2**3)
+print(2**5)
+print(2**10)
+print(111**111)
+2**5
+print('hola mundo)
+print('hola ' *3)
+
+if True:
+    print("Hola")
+else:
+    print("Chao")
+
+class OB():
+    pass
+
+def fun ():
+    pass
